ab96183b26525bf097639dcc385942500d94f1d9
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-02-15 15:41:32 +0100

    details

--- bin/npo_check_credentials.py
+++ bin/npo_check_credentials.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 """
-  Simple client to get an object from the NPO Frontend API media endpoint
+  Simple client to get an object from the NPO Frontend API media endpoint. This version accepts explicit key, secret origins.
 """
 from npoapi import Media
 import argparse
 
 ARGS = argparse.ArgumentParser(
-    description="Get an media object from the NPO Frontend API",
-    epilog=Media.EPILOG
+    description="Get an media object from the NPO Frontend API using provided credentials. This lets you easily check whether new credentials do work"
+
 )
 ARGS.add_argument('apikey', type=str, nargs=1, help='key')
 ARGS.add_argument('apisecret', type=str, nargs=1, help='secret')
 ARGS.add_argument('origin', type=str, nargs=1, help='origin')
+ARGS.add_argument('mid', type=str, nargs='?', help='mid', default="WO_NCRV_026201")
 
 args = ARGS.parse_args()
 client = Media(key=args.apikey[0], secret=args.apisecret[0], origin=args.origin[0]).read_environmental_variables()
 
-print(client.get("WO_NCRV_026201"))
+mid = args.mid
+
+print(client.get(mid))
