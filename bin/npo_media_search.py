90f8cccd2f0609fdd3e69afc70e0296a9b359fe6
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2016-03-23 15:22:49 +0100

    help

--- bin/npo_media_search.py
+++ bin/npo_media_search.py
@@ -5,10 +5,10 @@
 from npoapi import Media
 client = Media().command_line_client(description="Search from the NPO Frontend API")
 
-client.ARGS.add_argument('form', type=str, nargs=1, help='Form')
+client.ARGS.add_argument('form', type=str, nargs=1, help='The search form. This may be a json string, or the name of a file containing it')
 client.ARGS.add_argument('-s', "--sort", type=str, default=None, choices={"asc", "desc"},
                          help="sort (only relevant when using sub)")
-client.ARGS.add_argument('-m', "--max", type=int, default=240)
+client.ARGS.add_argument('-m', "--max", type=int, default="240")
 client.ARGS.add_argument('-o', "--offset", type=int, default=0)
 args = client.parse_args()
 print(client.search(args.form[0], sort=args.sort, max_=args.max, offset=args.offset))
