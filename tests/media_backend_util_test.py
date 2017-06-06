d0582b06eb9d2c94e0fb99ab5b63221567d6487a
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-06 11:04:35 +0200

    Test case failures.

--- tests/media_backend_util_test.py
+++ tests/media_backend_util_test.py
@@ -12,14 +12,14 @@ class Tests(unittest.TestCase):
         image = MU.create_image("http://www.vpro.nl/1.jpg", title="hoi")
         print(image.toxml())
         self.assertEquals("""
-        <?xml version="1.0" ?><image highlighted="false" type="PICTURE" xmlns="urn:vpro:media:update:2009"><title>hoi</title><imageLocation><url>http://www.vpro.nl/1.jpg</url></imageLocation></image>
+       <?xml version="1.0" ?><image highlighted="false" type="PICTURE" xmlns="urn:vpro:media:update:2009"><title>hoi</title><license>COPYRIGHTED</license><imageLocation><url>http://www.vpro.nl/1.jpg</url></imageLocation></image>
         """.strip(), image.toxml())
 
     def test_create_image_from_file(self):
         file = os.path.join(os.path.dirname(__file__), "1.gif")
         image = MU.create_image_from_file(file, title="hoi")
         self.assertEquals("""
-        <?xml version="1.0" ?><image highlighted="false" type="PICTURE" xmlns="urn:vpro:media:update:2009"><title>hoi</title><imageData><data>R0lGODdhGAAQAKEAAP8AAP///wAA/wAAACwAAAAAGAAQAAACIISPqcvtD6OclIWLs968+w+G4kgK5omm6sq27gvH8lwAADs=</data></imageData></image>
+        <?xml version="1.0" ?><image highlighted="false" type="PICTURE" xmlns="urn:vpro:media:update:2009"><title>hoi</title><license>COPYRIGHTED</license><imageData><data>R0lGODdhGAAQAKEAAP8AAP///wAA/wAAACwAAAAAGAAQAAACIISPqcvtD6OclIWLs968+w+G4kgK5omm6sq27gvH8lwAADs=</data></imageData></image>
         """.strip(), image.toxml())
 
     def test_format_duration(self):
