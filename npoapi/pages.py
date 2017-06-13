3783988916e5a4e793ad925465e9bbd05065c349
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2017-06-13 16:03:54 +0200

    Support for iterate call (@since 5.4)

--- npoapi/pages.py
+++ npoapi/pages.py
@@ -8,3 +8,7 @@ class Pages(NpoApi):
 
     def search(self, form="{}", sort="asc", offset=0, limit=240, profile=None, accept=None) -> str:
         return self.request("/api/pages", data=form, accept=accept, params={"sort": sort, "offset": offset, "max": limit, "profile": profile})
+
+    def iterate(self, form="{}", profile=None, offset=0, limit=None) ->str:
+        import ijson
+        return ijson.items(self.stream("/api/pages/iterate", data=form, accept="application/json", params={"profile": profile, "offset": offset, "max": limit}), "pages.item")
