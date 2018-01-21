d4802f4bd4fcc1b76bb4e1c9a6d1bc29a1ab2601
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-01-21 17:11:22 +0100

    fixed dependency versions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='NPO API',
     version=__version__,
     packages=['npoapi', 'npoapi.xml'],
-    install_requires=['pytz', 'pyxb==1.2.6'],
+    install_requires=['pytz==2017.3', 'ijson==2.3', 'pyxb==1.2.6'],
     scripts=[
         'bin/npo_media_get',
         'bin/npo_media_search',
