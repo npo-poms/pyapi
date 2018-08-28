5e7e5f2892c37db70c8ee7d3468d700281c1db23
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2018-08-28 13:12:25 +0200

    schedule call with cahnnel not working

--- npoapi/schedule.py
+++ npoapi/schedule.py
@@ -1,12 +1,13 @@
 from npoapi.npoapi import NpoApi
 
+
 class Schedule(NpoApi):
     def get(self, guideDay=None, channel=None):
         params = {
             'guideDay': guideDay
         }
         if channel:
-            return self.request("/api/schedule/" + channel, params=params)
+            return self.request("/api/schedule/channel/" + channel, params=params)
         else:
             return self.request("/api/schedule", params=params)
 
