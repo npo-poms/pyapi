a4acb075e0ce17ea98ed2d3b89e3c70ff6ba25d8
Michiel Meeuwissen <git@meeuw.org>
2021-10-22 10:25:25 +0200

    Provided entrypoint to make npo_ optional

---
+++ docker/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# simple script to make the npo_ prefixes superflouous
+command=$1
+shift
+if case $command in  "npo_"*) true;; *) false;; esac; then
+  $command "$@"
+else
+  npo_$command "$@"
+fi
