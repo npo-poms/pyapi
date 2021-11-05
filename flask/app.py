567c5a152e7750c68181e7875308c6a0d79050f4
Michiel Meeuwissen <git@meeuw.org>
2021-11-05 23:05:55 +0100

    Adding examples, propertiesfiltering.

--- flask/app.py
+++ flask/app.py
@@ -2,6 +2,7 @@ import os
 
 from flask import Flask, render_template, send_from_directory, Response
 from npoapi import Media
+import json
 
 app = Flask(__name__)
 
@@ -13,7 +14,10 @@ client.env("prod")
 @app.route('/')
 @app.route('/<path:mid>')
 def index(mid = None):
-    return render_template('index.html', mid=mid)
+    # Opening JSON file
+    with open(os.fspath('examples.json')) as json_file:
+        examples = json.load(json_file)
+    return render_template('index.html', mid=mid, examples=examples)
 
 @app.route('/js/<path:path>')
 def send_js(path):
@@ -22,7 +26,7 @@ def send_js(path):
 
 @app.route('/mediaobject/<path:mid>')
 def send_mediaobject(mid):
-    proxied_response = client.get(mid, accept="application/json", stream=True, properties=["titles", "locations", "predictions"])
+    proxied_response = client.get(mid, accept="application/json", stream=True, properties=["titles", "locations", "predictionsForXml"])
     response = Response(proxied_response, mimetype='application/json')
     return response
 
