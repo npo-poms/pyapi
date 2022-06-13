942ac79d2d9ce1a42d2b60dda0bf1005d23895e0
Michiel Meeuwissen <git@meeuw.org>
2022-06-13 09:20:16 +0200

    Added help when no arguments.
    Also improved README (Using -it is no good).

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,6 +2,28 @@
 # simple script to make the npo_ prefixes superflouous
 command=$1
 shift
+if [ "$command" == "" ] ; then
+    echo << EOF
+No argument given. Try e.g. following arguments.
+# frontend apis
+media_get  --help
+media_changes  --help
+media_follow_changes  --help
+media_iterate  --help
+media_search  --help
+pages_get  --help
+pages_search  --help
+schedule_get  --help
+schedule_search  --help
+check_credentials  --help
+
+# backend apis
+mediabackend  --help
+mediabackend_get  --help
+pagesbackend  --help
+EOF
+    exit
+fi
 if case $command in  "npo_"*) true;; *) false;; esac; then
   $command "$@"
 elif [ $(which "npo_$command" > /dev/null ; echo $?)  = 0 ]; then
