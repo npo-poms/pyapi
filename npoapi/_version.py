8cca7085e1e598e345a3dcdadb459b6c50391907
Michiel Meeuwissen <git@meeuw.org>
2022-11-01 08:50:23 +0100

    small hack to make it work with newer python version

--- npoapi/_version.py
+++ npoapi/_version.py
@@ -1,1 +1,7 @@
 __version__ = "7.1.0.dev"
+
+# would this make pyxb work with python 3.10
+import collections
+if not(hasattr(collections, "MutableSequence")):
+    from collections.abc import MutableSequence
+    collections.MutableSequence = collections.abc.MutableSequence
