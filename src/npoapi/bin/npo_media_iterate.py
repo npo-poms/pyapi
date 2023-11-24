8bc6a8cc25f31ecd9121909b2744b2380bdf9dd1
Michiel Meeuwissen <michiel.meeuwissen@gmail.com>
2023-11-24 09:31:24 +0100

    Clarified help

--- src/npoapi/bin/npo_media_iterate.py
+++ src/npoapi/bin/npo_media_iterate.py
@@ -10,6 +10,7 @@ import os
 
 
 def media_iterate():
+    buffer_size = 10000
     client = Media()\
         .command_line_client(description="Iterate the NPO Frontend API. This is the adviced way to download all data or all data relevant to you (via profile and/or form arguments).", exclude_arguments= {"accept"})
     client.add_argument('profile', type=str, nargs='?',
@@ -17,7 +18,7 @@ def media_iterate():
     client.add_argument('form', type=str, nargs='?',
                         help='The search form. This may be a json string, or the name of a file containing it')
     client.add_argument('-m', "--max", type=int, default="100", help="On default the size is maximized to 100, but unlike with other API calls you can set this max value arbitrary large. -1 means no maximum")
-    client.add_argument("--progress", action='store_true', help="If set to true, some progress indication will be written to stderr")
+    client.add_argument("--progress", action='store_true', help="If set to true, some progress indication will be written to stderr (a dot for every %s bytes)" % buffer_size)
     client.add_argument('-p', "--properties", type=str, default=None,   help="properties filtering")
 
     args = client.parse_args()
@@ -25,7 +26,7 @@ def media_iterate():
 
     response = client.iterate_raw(form=form, profile=args.profile, limit=None if args.max == -1 else args.max, timeout=100, properties=args.properties)
 
-    buffer_size = 10000
+
     buffer = bytearray("-" * buffer_size, "ascii")
     total_count = 0
     while not response.closed:
